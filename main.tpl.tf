d472b0bc0e733e35cdca551b3e625a537ba6663f
Nikita Mastukov <nikita@mstkv.ru>
2020-02-06 18:23:00 +0300

    Update main.tpl.tf

--- main.tpl.tf
+++ main.tpl.tf
@@ -3,10 +3,3 @@ provider "aws" {
   access_key = "{$ .P.aws_access_key $}"
   secret_key = "{$ .P.aws_secret_key $}"
 }
-
-terraform {
-  backend "pg" {
-    conn_str = "postgres://terraform:{$ .P.pg_password $}@pgsql-main.service.{$ .P.Datacenter $}.consul/terraform_backend"
-    schema_name = "{$ .P.tf_workspace $}"
-  }
-}
