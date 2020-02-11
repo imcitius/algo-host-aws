77306dac06a65751e321bd64c8a0242a70338db1
ilya <ilya@2cit.ru>
2020-02-11 16:34:34 +0300

    consul state

--- main.tpl.tf
+++ main.tpl.tf
@@ -4,9 +4,17 @@ provider "aws" {
   secret_key = "{$ .P.aws_secret_key $}"
 }
 
+// terraform {
+//   backend "pg" {
+//     conn_str = "postgres://terraform:{$ .P.pg_password $}@pgsql-main.service.{$ .P.Datacenter $}.consul/terraform_backend"
+//     schema_name = "{$ .P.tf_workspace $}"
+//   }
+// }
+
 terraform {
-  backend "pg" {
-    conn_str = "postgres://terraform:{$ .P.pg_password $}@pgsql-main.service.{$ .P.Datacenter $}.consul/terraform_backend"
-    schema_name = "{$ .P.tf_workspace $}"
+  backend "consul" {
+    address = "consul.service.infra1.consul:8500"
+    scheme  = "http"
+    path    = "tf/states/faceless-algo-test%3A{$ .P.tf_workspace $}"
   }
 }
